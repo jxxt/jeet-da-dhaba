2b0db9933b0c645143727e0ce1033882e0fc1978
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-06-05 18:23:09 +0530

    added assets loader in webpack

--- src/home.js
+++ src/home.js
@@ -1,10 +1,13 @@
+import homeImg from './home.jpeg'
+
 const home = () => {
+
     const content = document.querySelector(".content")
 
     const homeElement = document.createElement("div")
     homeElement.classList.add("home")
     homeElement.classList.add("hide")
-    homeElement.innerHTML= `<h1>!! welcome to jeet's imaginary metaverse dhaba !!</h1><div class="home-img"><img src="./assets/home.jpeg" alt=""></div>`
+    homeElement.innerHTML = `<h1>!! welcome to jeet's imaginary metaverse dhaba !!</h1><div class="home-img"><img src="${homeImg}" alt=""></div>`
 
     content.appendChild(homeElement)
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import './styles.css';
 import button from "./buttons";
 import heading from './heading';
 import home from './home';
+import menu from './menu';
 
 button()
 heading()
 home()
+menu()
